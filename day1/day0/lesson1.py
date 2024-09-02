afb2e8ca64de7b994d04bc1a610cec2d52ce4737
Dadu2012 <daduakobia@gmail.com>
2024-09-02 14:29:40 +0400

    first commit

---
+++ day1/day0/lesson1.py
@@ -0,0 +1,31 @@
+
+
+#name  არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო 
+# "dadu" არის ცვლადისთვის მნიშვნელობა 
+
+
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+surname ='akobia'
+name = "dadu"  #ეს არის str (string)  ტიპის ცვლადი
+age = 12 #ეს არის int (integer) მთელი რიცხვი
+height =151 #ეს არის float ტიპისცვლადი(ათწილადი)
+#booLean(bool) ტიპის ცვლადი
+
+
+knows_programming = True #true ან false
+is_ugly = False
+
+
+
+print(name +  ' ' +str (age)+' ' +str (surname) +' '+ str (height ) )
+
+
+
+
+
+
+
+
+
